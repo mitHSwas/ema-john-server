0da2aa8e896dc6fcee2ee9d7bea76d41b4329df4
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-11-05 01:40:08 +0600

    ema john more banaia desha ammhajan

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,12 +18,24 @@ async function run() {
     try {
         const productCollection = client.db("emaJohn").collection("products");
 
-        app.get('/orders', async (req, res) => {
+        app.get('/products', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log(page, size);
             const query = {};
             const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
+            const products = await cursor.skip(page * size).limit(size).toArray();
             const count = await productCollection.estimatedDocumentCount();
             res.send({ count, products });
+        })
+
+        app.post('/productsByIds', async (req, res) => {
+            const ids = req.body;
+            const objectIds = ids.map(id => ObjectId(id))
+            const query = { _id: { $in: objectIds } };
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
         })
     }
     finally {
